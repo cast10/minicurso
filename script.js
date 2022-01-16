2c72b961eb3ca0bc87b341e67e07315fc5bcef46
Douglas Castanha <castanha.douglas2013@gmail.com>
2022-01-15 22:16:38 -0300

    alterar a imagem para o centro da tela

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const imagem = document.querySelector('img');
-const botao = document.querySelector ('#botao');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+
+
+
+
+
 
 gerarvalorAleatorio =() => {
     return Math.floor(Math.random() *671)
-
 
 }
 
 
 pegarPersonagem = () => {
     let numeroAleatorio = gerarvalorAleatorio();
+    
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers:{
@@ -23,16 +23,14 @@ pegarPersonagem = () => {
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
         especie.innerHTML = data.species;
         condicao.innerHTML = data.status;
     });
 
+
     
 }
-
-    
-
-
 
 botao.onclick = pegarPersonagem;
 
